053be37e000ad103db2dbd2557f6e01729975227
Endy <andriy05032@gmail.com>
2024-11-09 19:18:25 +0200

    add adaptive

--- js/main.js
+++ js/main.js
@@ -19,6 +19,18 @@ tabItem.forEach(function(element) {
     });
 });
 
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+});
+
+
+
+
+
+
 const swiper = new Swiper(".swiper", {
     effect: "fade", // Додає безкінечне повторення слайдів
     pagination: {
